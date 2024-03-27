612e7d82b5b7f2b5e36a87c2eba480032d20a21f
eggirr <vlad.grishanenko.g@mail.ru>
2024-03-27 22:16:34 +0700

    add tests

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -90,4 +90,92 @@ class UserTest extends TestCase
         $collect->pop();
         $this->assertSame([1, 2], $collect->toArray());
     }
+    public function testKeysNotEqual()
+    {
+        $collect = new Collect\Collect(['a' => 1, 'b' => 2]);
+        $this->assertNotEquals(['b', 'a'], $collect->keys()->toArray());
+    }
+
+    public function testValuesNotEqual()
+    {
+        $collect = new Collect\Collect(['a' => 1, 'b' => 2]);
+        $this->assertNotEquals([2, 1], $collect->values()->toArray());
+    }
+
+    public function testGetNotEqual()
+    {
+        $collect = new Collect\Collect(['a' => 1, 'b' => 2]);
+        $this->assertNotEquals(2, $collect->get('a'));
+    }
+
+    public function testExceptNotEqual()
+    {
+        $collect = new Collect\Collect(['a' => 1, 'b' => 2, 'c' => 3]);
+        $this->assertNotEquals(['b' => 3], $collect->except('a', 'c')->toArray());
+    }
+
+    public function testOnlyNotEqual()
+    {
+        $collect = new Collect\Collect(['a' => 1, 'b' => 2, 'c' => 3]);
+        $this->assertNotEquals(['a' => 1], $collect->only('a', 'b')->toArray());
+    }
+
+    public function testFirstNotEqual()
+    {
+        $collect = new Collect\Collect(['a' => 1, 'b' => 2, 'c' => 3]);
+        $this->assertNotEquals(2, $collect->first());
+    }
+
+    public function testSearchNotEqual()
+    {
+        $collect = new Collect\Collect([
+            ['id' => 1, 'name' => 'Alice'],
+            ['id' => 2, 'name' => 'Bob'],
+            ['id' => 3, 'name' => 'Alice']
+        ]);
+        $result = $collect->search('name', 'Alice')->toArray();
+        $this->assertNotEquals([['id' => 1, 'name' => 'Alice']], $result);
+    }
+
+    public function testMapNotEqual()
+    {
+        $collect = new Collect\Collect([1, 2, 3]);
+        $result = $collect->map(function ($item) {
+            return $item * 2;
+        })->toArray();
+        $this->assertNotEquals([1, 2, 3], $result);
+    }
+
+    public function testPushNotEqual()
+    {
+        $collect = new Collect\Collect(['a' => 1, 'b' => 2]);
+        $collect->push(3, 'c');
+        $this->assertNotEquals(['a' => 1, 'b' => 2], $collect->toArray());
+    }
+
+    public function testUnshiftNotEqual()
+    {
+        $collect = new Collect\Collect([2, 3]);
+        $collect->unshift(1);
+        $this->assertNotEquals([2, 3], $collect->toArray());
+    }
+
+    public function testShiftNotEqual()
+    {
+        $collect = new Collect\Collect([1, 2, 3]);
+        $collect->shift();
+        $this->assertNotEquals([1, 2, 3], $collect->toArray());
+    }
+
+    public function testPopNotEqual()
+    {
+        $collect = new Collect\Collect([1, 2, 3]);
+        $collect->pop();
+        $this->assertNotEquals([1, 2, 3], $collect->toArray());
+    }
+//    public function testGetNonExistingKey()
+//    {
+//        $collect = new Collect\Collect(['a' => 1, 'b' => 2]);
+//        $this->assertNull($collect->get('c'));
+//    }
 }
